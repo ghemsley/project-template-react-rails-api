ea7e136fb97e55e081ddd23e6279e39734e8a021
ghemsley <ghemsley@protonmail.ch>
2021-07-30 06:00:15 -0500

    Add categories reducer file

---
+++ client/src/reducers/categories.js
@@ -0,0 +1,39 @@
+import uuid from 'uuid'
+
+const categories = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_CATEGORY':
+      console.log('creating category')
+      return [
+        ...state,
+        {
+          ...action.payload,
+          id: uuid()
+        }
+      ]
+
+    case 'UPDATE_CATEGORY':
+      console.log('updating category')
+      const newState = [...state]
+      const currentCategory = newState.find(
+        category => category.id === action.payload.id
+      )
+      if (currentCategory) {
+        for (const key in action.payload) {
+          if (key !== 'id') {
+            currentCategory[key] = action.payload[key]
+          }
+        }
+      }
+      return newState
+
+    case 'DELETE_CATEGORY':
+      console.log('deleting category')
+      return [...state.filter(category => category.id !== action.payload.id)]
+
+    default:
+      return state
+  }
+}
+
+export default categories
