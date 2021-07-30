13f76ea7bfab32bcbe8288e95b25ea5606296422
ghemsley <ghemsley@protonmail.ch>
2021-07-30 06:04:04 -0500

    Add routing, navbar, pages to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css'
+import { Navbar } from './components/index'
+import Pages from './pages/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='pure-u-1'>
+        <Navbar />
+        <Switch>
+          {Pages.map((page, i) => (
+            <Route exact path={page.path} key={i}>
+              {page.component()}
+            </Route>
+          ))}
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
