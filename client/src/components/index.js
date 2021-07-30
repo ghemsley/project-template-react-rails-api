5e838d07351dae77c43fa16220092fc9dca6cf1f
ghemsley <ghemsley@protonmail.ch>
2021-07-30 06:11:22 -0500

    Add components/index.js

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Hover from './Hover'
+import Navbar from './Navbar'
+import Category from './Category'
+import CategoryForm from './CategoryForm'
+import Todo from './Todo'
+import TodoForm from './TodoForm'
+import Project from './Project'
+import ProjectForm from './ProjectForm'
+
+
+export { Hover, Navbar, Category, CategoryForm, Todo, TodoForm, Project, ProjectForm }
