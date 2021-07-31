dbcf9bbcb2ca8940b4bb6cc138be74c16d5e58ed
ghemsley <ghemsley@protonmail.ch>
2021-07-31 12:59:16 -0500

    Add DnDProvider to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 import './App.css'
 import { Navbar } from './components/index'
 import Pages from './pages/index'
@@ -6,16 +8,18 @@ import Pages from './pages/index'
 function App() {
   return (
     <Router>
-      <div className='pure-u-1'>
-        <Navbar />
-        <Switch>
-          {Pages.map((page, i) => (
-            <Route exact path={page.path} key={i}>
-              {page.component()}
-            </Route>
-          ))}
-        </Switch>
-      </div>
+      <DndProvider backend={HTML5Backend}>
+        <div className='pure-u-1'>
+          <Navbar />
+          <Switch>
+            {Pages.map((page, i) => (
+              <Route exact path={page.path} key={i}>
+                {page.component()}
+              </Route>
+            ))}
+          </Switch>
+        </div>
+      </DndProvider>
     </Router>
   )
 }
