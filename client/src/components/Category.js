a78e20e78a64307b77a3148a6b0236a7684ca247
ghemsley <ghemsley@protonmail.ch>
2021-07-31 13:09:16 -0500

    Integrate react-dnd with Category component

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,7 +1,15 @@
-import React, { useCallback, useLayoutEffect, useRef, useState } from 'react'
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react'
+import { useDrag } from 'react-dnd'
 import { useDispatch, useSelector } from 'react-redux'
 import actions from '../actions/index'
-import { Todo, Hover, Project } from './index'
+import { Todo, Hover, Project, Dropzone } from './index'
 
 const Category = props => {
   const todos = useSelector(state =>
@@ -9,171 +17,105 @@ const Category = props => {
   )
   const projects = useSelector(state => state.projects)
   const dispatch = useDispatch()
-  const [dragging, setDragging] = useState(false)
-  const [left, setLeft] = useState(0)
-  const [top, setTop] = useState(0)
-  const [leftOffset, setLeftOffset] = useState(0)
-  const [topOffset, setTopOffset] = useState(0)
-  const categoryRef = useRef(null)
+  const coordinatesRef = useRef(null)
 
-  const handlePointerDown = event => {
-    event.stopPropagation()
-    if (props.draggable) {
-      setDragging(true)
-      setLeft(event.clientX)
-      setTop(event.clientY)
-    }
-  }
-
-  const handleButtonPointerDown = event => {
-    event.stopPropagation()
+  const handleButtonPointerDown = () => {
     dispatch(actions.deeplyDeleteCategory({ id: props.category.id }))
   }
 
-  const handlePointerMove = useCallback(
-    event => {
-      setLeft(event.clientX)
-      setTop(event.clientY)
-    },
-    []
-  )
-
-  const handlePointerUp = useCallback(
-    event => {
-      setLeft(event.clientX)
-      setTop(event.clientY)
-      if (props.draggable) {
-        for (const project of projects) {
-          const element = document.getElementById(project.id)
-          if (element) {
-            const rect = element.getBoundingClientRect()
-            const coordinates = {
-              id: project.id,
-              left: rect.left,
-              right: rect.right,
-              top: rect.top,
-              bottom: rect.bottom,
-              height: rect.height,
-              width: rect.width
-            }
-            dispatch(
-              actions.updateProjectCoordinates({
-                coordinates
-              })
-            )
-          }
-        }
-        dispatch(
-          actions.updateCategoryAssociation({
-            category: props.category,
-            left,
-            top
-          })
-        )
-        setDragging(false)
-      }
-    },
-    [props.category, props.draggable, projects, left, top, dispatch]
-  )
-
   useLayoutEffect(() => {
-    if (categoryRef.current) {
-      const rect = categoryRef.current.getBoundingClientRect()
+    if (coordinatesRef.current) {
+      const rect = coordinatesRef.current.getBoundingClientRect()
       const coordinates = {
-        id: props.category.id,
         left: rect.left,
         right: rect.right,
         top: rect.top,
-        bottom: rect.bottom,
-        height: rect.height,
-        width: rect.width
+        bottom: rect.bottom
       }
-      dispatch(
-        actions.updateCategoryCoordinates({
-          coordinates
-        })
-      )
-    }
-    if (props.draggable) {
-      if (categoryRef.current && dragging) {
-        setLeftOffset(categoryRef.current.clientWidth / 2)
-        setTopOffset(categoryRef.current.clientHeight / 2)
-      }
-      if (dragging) {
-        document.addEventListener('pointermove', handlePointerMove, {
-          passive: true
-        })
-        document.addEventListener('pointerup', handlePointerUp, {
-          passive: true
-        })
-      }
-      return () => {
-        document.removeEventListener('pointerup', handlePointerUp)
-        document.removeEventListener('pointermove', handlePointerMove)
+      if (coordinates !== props.category.coordinates) {
+        dispatch(actions.updateCategory({ ...props.category, coordinates }))
       }
     }
-  }, [
-    props.category.id,
-    props.draggable,
-    dragging,
-    handlePointerUp,
-    handlePointerMove,
-    dispatch
-  ])
+  }, [props.category, dispatch])
+
+  const [{ dragging, position }, drag, dragPreview] = useDrag(() => ({
+    type: 'CATEGORY',
+    item: props.category,
+    previewOptions: {captureDraggingState: true},
+    collect: monitor => ({
+      dragging: monitor.isDragging(),
+      position: monitor.getSourceClientOffset()
+    })
+  }))
+
+  const style = useMemo(
+    () => ({ background: dragging ? '#1070b0' : '#1060a0' }),
+    [dragging]
+  )
+
+  const handleDrop = (item, position) => {
+    dispatch(actions.updateTodo({ ...item, categoryID: props.category.id }))
+  }
 
   return (
     <div
       id={props.category.id}
-      className='hoverable flex-child radius-6 category'
+      className='hoverable flex-child radius-4 category'
       style={{
-        background: '#1166aa',
         color: 'whitesmoke',
-        position: dragging ? 'fixed' : 'unset',
-        left: dragging ? left - leftOffset : 'unset',
-        top: dragging ? top - topOffset : 'unset'
+        transition: 'background-color 50ms ease',
+        ...style
       }}
-      onPointerDown={handlePointerDown}
-      ref={categoryRef}
+      ref={drag}
     >
-      <h2>{props.category.name}</h2>
-      <p>{props.category.description}</p>
-      {props.showDelete && !dragging && (
-        <button
-          className='pure-button pure-button-primary animate-height'
-          onPointerDown={handleButtonPointerDown}
-        >
-          Delete
-        </button>
-      )}
-      {props.showTodos && (
-        <div>
-          <h3>Todos</h3>
-          {todos.length < 1 && (
-            <p style={{ fontSize: '14px' }}>Place todos here!</p>
+      <div ref={coordinatesRef}>
+        <Dropzone handleDrop={handleDrop} acceptType='TODO'>
+          <h2>{props.category.name}</h2>
+          <p>{props.category.description}</p>
+          {props.showDelete && !dragging && (
+            <button
+              className='pure-button pure-button-primary animate-height'
+              onClick={handleButtonPointerDown}
+            >
+              Delete
+            </button>
           )}
-          <div className='flex'>
-            {todos.map((todo, i) => (
-              <Todo
-                todo={todo}
-                draggable={true}
-                showDelete={props.showDelete && !dragging}
-                parentRef={categoryRef}
-                key={i}
-              />
-            ))}
-          </div>
-        </div>
-      )}
-      {props.hoverable && !dragging && (
-        <Hover>
-          <Project
-            project={projects.find(
-              project => project.id === props.category.projectID
-            )}
-            showCategories={true}
-          />
-        </Hover>
-      )}
+          {props.showTodos && (
+            <>
+              {todos.length < 1 ? (
+                <>
+                  <h3>Todos</h3>
+                  <p style={{ fontSize: '14px' }}>Drop todos here!</p>
+                </>
+              ) : (
+                <>
+                  <h3>Todos</h3>
+                  <div className='flex parent'>
+                    {todos.map(todo => (
+                      <Todo
+                        todo={todo}
+                        draggable={true}
+                        showDelete={props.showDelete && !dragging}
+                        key={todo.id}
+                      />
+                    ))}
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </Dropzone>
+        {props.hoverable && !dragging && (
+          <Hover>
+            <Project
+              project={projects.find(
+                project => project.id === props.category.projectID
+              )}
+              showCategories={true}
+            />
+          </Hover>
+        )}
+      </div>
     </div>
   )
 }
--- client/src/reducers/categories.js
+++ client/src/reducers/categories.js
@@ -1,4 +1,4 @@
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
 
 const categories = (state = [], action) => {
   switch (action.type) {
