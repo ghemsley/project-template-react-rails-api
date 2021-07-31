43611306e2c04dd659f25fac14e1f1f678c8e285
ghemsley <ghemsley@protonmail.ch>
2021-07-31 13:09:46 -0500

    Integrate react-dnd with Project component

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,89 +1,82 @@
-import React, { useLayoutEffect, useRef } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import actions from '../actions/index'
-import { Category, Hover } from './index'
+import { Category, Hover, Dropzone } from './index'
 
 const Project = props => {
   const categories = useSelector(state =>
     state.categories.filter(category => category.projectID === props.project.id)
   )
   const dispatch = useDispatch()
-  const projectRef = useRef(null)
-
-  useLayoutEffect(() => {
-    if (projectRef.current) {
-      const rect = projectRef.current.getBoundingClientRect()
-      const coordinates = {
-        id: props.project.id,
-        left: rect.left,
-        right: rect.right,
-        top: rect.top,
-        bottom: rect.bottom,
-        height: rect.height,
-        width: rect.width
-      }
+  const coordinatesRef = useRef(null)
+  const style = useMemo(() => ({}), [])
+  const handleClick = () => {
+    dispatch(actions.deeplyDeleteProject(props.project))
+  }
+  const handleDrop = (item, cursor, coordinates) => {
+    if (item.projectID !== props.project.id) {
       dispatch(
-        actions.updateProjectCoordinates({
-          coordinates
+        actions.updateCategory({
+          ...item,
+          projectID: props.project.id
         })
       )
     }
-  }, [props.project.id, dispatch])
-
-  const handleClick = () => {
-    dispatch(actions.deeplyDeleteProject(props.project))
   }
 
   return (
     <div
       id={props.project.id}
-      className='hoverable flex-child radius-6 project'
+      className='hoverable flex-child radius-4 project'
       style={{
-        background: '#11aa99',
-        color: 'whitesmoke'
+        background: '#10a090',
+        color: 'whitesmoke',
+        ...style
       }}
-      ref={projectRef}
     >
-      <h2>{props.project.name}</h2>
-      <p>{props.project.description}</p>
-      {props.hoverable && categories.length > 0 && (
-        <Hover>
-          {categories.map((category, i) => (
-            <div key={i}>
-              <Category category={category} />
-              {categories[i + 1] && <hr />}
-            </div>
-          ))}
-        </Hover>
-      )}
-      {props.showDelete && (
-        <button
-          className='pure-button pure-button-primary animate-height'
-          onClick={handleClick}
-        >
-          Delete
-        </button>
-      )}
-      {props.showCategories && (
-        <div>
-          <h3>Categories</h3>
-          {categories.length < 1 && (
-            <p style={{ fontSize: '14px' }}>Place categories here!</p>
+      <div ref={coordinatesRef}>
+        <Dropzone handleDrop={handleDrop} acceptType='CATEGORY'>
+          <h2>{props.project.name}</h2>
+          <p>{props.project.description}</p>
+          {props.showDelete && (
+            <button
+              className='pure-button pure-button-primary animate-height'
+              onClick={handleClick}
+            >
+              Delete
+            </button>
           )}
-          <div className='flex'>
+          {props.showCategories && (
+            <div>
+              <h3>Categories</h3>
+              {categories.length < 1 && (
+                <p style={{ fontSize: '14px' }}>Place categories here!</p>
+              )}
+              <div className='flex parent'>
+                {categories.map(category => (
+                  <Category
+                    category={category}
+                    showTodos={true}
+                    showDelete={props.showDelete}
+                    draggable={true}
+                    items={props.items}
+                    key={category.id}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </Dropzone>
+        {props.hoverable && categories.length > 0 && (
+          <Hover>
             {categories.map((category, i) => (
-              <Category
-                category={category}
-                showTodos={true}
-                showDelete={props.showDelete}
-                draggable={true}
-                parentRef={projectRef}
-                key={i}
-              />
+              <div key={i}>
+                <Category category={category} />
+              </div>
             ))}
-          </div>
-        </div>
-      )}
+          </Hover>
+        )}
+      </div>
     </div>
   )
 }
