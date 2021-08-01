1a84a2aa8a63f10e17ed7f6da4194cd447ad16c3
ghemsley <ghemsley@protonmail.ch>
2021-07-31 19:36:08 -0500

    Add modal link to projects page

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import ProjectForm from '../components/ProjectForm'
 import Project from '../components/Project'
+import { Link, Route, useLocation } from 'react-router-dom'
 
 const Projects = () => {
   const projects = useSelector(state => state.projects)
+  const location = useLocation()
 
   return (
     <div className='center center-text project-page'>
       <div>
-        <p>Create Project</p>
-        <ProjectForm />
-        <p>Projects</p>
+        <Link className='pure-button pure-button-primary'
+          to={{ pathname: 'projects/new', state: { background: location } }}
+        >
+          Create Project
+        </Link>
+        {projects.length > 0 && <h1>Projects</h1>}
       </div>
       <div className='flex project-container'>
         {projects.map(project => (
