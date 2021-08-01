04fc649fdae68dd6e6f752b1527381f8b2f946c9
ghemsley <ghemsley@protonmail.ch>
2021-07-31 19:34:55 -0500

    Update project form to use modal

--- client/src/components/ProjectForm.js
+++ client/src/components/ProjectForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import actions from '../actions/index'
+import Modal from './Modal'
 
-const ProjectForm = () => {
+const ProjectForm = (props) => {
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const dispatch = useDispatch()
@@ -33,25 +34,28 @@ const ProjectForm = () => {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='pure-form pure-form-stacked center fit'
-    >
-      <fieldset>
-        <label htmlFor='name'>Name</label>
-        <input type='text' name='name' value={name} onChange={handleChange} />
-        <label htmlFor='description'>Description</label>
-        <input
-          type='text'
-          name='description'
-          value={description}
-          onChange={handleChange}
-        />
-        <button className='pure-button pure-button-primary' type='submit'>
-          Submit
-        </button>
-      </fieldset>
-    </form>
+    <Modal>
+      <h1 className='fit margin-auto'>New Project</h1>
+      <form
+        onSubmit={handleSubmit}
+        className='pure-form pure-form-stacked fit margin-auto'
+      >
+        <fieldset>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' value={name} onChange={handleChange} />
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            name='description'
+            value={description}
+            onChange={handleChange}
+          />
+          <button className='pure-button pure-button-primary' type='submit'>
+            Submit
+          </button>
+        </fieldset>
+      </form>
+    </Modal>
   )
 }
 export default ProjectForm
