f42927f43a2fcbf6f3401530d23f49d7d20b453a
ghemsley <ghemsley@protonmail.ch>
2021-07-31 19:34:40 -0500

    Update category form to use modal

--- client/src/components/CategoryForm.js
+++ client/src/components/CategoryForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import actions from '../actions/index'
-import Hover from './Hover'
-import Project from './Project'
+import { Hover, Project, Modal } from './index'
 
 const CategoryForm = () => {
   const projects = useSelector(state => state.projects)
@@ -43,58 +42,63 @@ const CategoryForm = () => {
   }
 
   return (
-    <>
+    <Modal>
       {projects.length < 1 ? (
-        <p>Please create a project first before trying to create a category!</p>
+        <p className='fit margin-auto'>Please create a project first before trying to create a category!</p>
       ) : (
-        <form
-          onSubmit={handleSubmit}
-          className='pure-form pure-form-stacked center fit'
-        >
-          <fieldset>
-            <label htmlFor='name'>Name</label>
-            <input
-              type='text'
-              name='name'
-              value={name}
-              onChange={handleChange}
-            />
-            <label htmlFor='description'>Description</label>
-            <input
-              type='text'
-              name='description'
-              value={description}
-              onChange={handleChange}
-            />
-            <label htmlFor='projectID' className='hoverable'>
-              Project
-              <select
-                name='projectID'
-                value={projectID}
+        <>
+          <h1 className='fit margin-auto'>New Category</h1>
+          <form
+            onSubmit={handleSubmit}
+            className='pure-form pure-form-stacked fit margin-auto'
+          >
+            <fieldset>
+              <label htmlFor='name'>Name</label>
+              <input
+                type='text'
+                name='name'
+                value={name}
                 onChange={handleChange}
-              >
-                {projects.map((project, i) => (
-                  <option value={project.id} key={i}>
-                    {project.name}
-                  </option>
-                ))}
-              </select>
-              {projects.length > 0 && (
-                <Hover>
-                  <Project
-                    project={projects.find(project => project.id === projectID)}
-                    showCategories={true}
-                  />
-                </Hover>
-              )}
-            </label>
-            <button className='pure-button pure-button-primary' type='submit'>
-              Submit
-            </button>
-          </fieldset>
-        </form>
+              />
+              <label htmlFor='description'>Description</label>
+              <input
+                type='text'
+                name='description'
+                value={description}
+                onChange={handleChange}
+              />
+              <label htmlFor='projectID' className='hoverable'>
+                Project
+                <select
+                  name='projectID'
+                  value={projectID}
+                  onChange={handleChange}
+                >
+                  {projects.map((project, i) => (
+                    <option value={project.id} key={i}>
+                      {project.name}
+                    </option>
+                  ))}
+                </select>
+                {projects.length > 0 && (
+                  <Hover>
+                    <Project
+                      project={projects.find(
+                        project => project.id === projectID
+                      )}
+                      showCategories={true}
+                    />
+                  </Hover>
+                )}
+              </label>
+              <button className='pure-button pure-button-primary' type='submit'>
+                Submit
+              </button>
+            </fieldset>
+          </form>
+        </>
       )}
-    </>
+    </Modal>
   )
 }
 export default CategoryForm
