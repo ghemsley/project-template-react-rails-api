7d6807584315200f622c3a863e86a0e34caae420
ghemsley <ghemsley@protonmail.ch>
2021-08-01 11:46:29 -0500

    Add todo fetch actions

--- client/src/actions/todos.js
+++ client/src/actions/todos.js
@@ -1,23 +1,44 @@
-const createTodo = (payload) => ({
+import constants from '../constants'
+
+const fetchTodo = payload => () => {
+  return fetch(`${constants.urls.BASE_URL}/todos/${payload.id}`, {
+    headers: { Accept: 'application/json' }
+  }).then(response => response.json())
+}
+
+const fetchTodos = () => () => {
+  return fetch(`${constants.urls.BASE_URL}/todos`, {
+    headers: { Accept: 'application/json' }
+  }).then(response => response.json())
+}
+
+const createTodo = payload => ({
   type: 'CREATE_TODO',
   payload
 })
 
-const updateTodo = (payload) => ({
+const updateTodo = payload => ({
   type: 'UPDATE_TODO',
   payload
 })
 
-const deleteTodo = (payload) => ({
+const deleteTodo = payload => ({
   type: 'DELETE_TODO',
   payload
 })
 
-const deleteTodosByCategory = (payload) => ({
+const deleteTodosByCategory = payload => ({
   type: 'DELETE_TODOS_BY_CATEGORY',
   payload
 })
 
-const todoActions = { createTodo, updateTodo, deleteTodo, deleteTodosByCategory }
+const todoActions = {
+  fetchTodo,
+  fetchTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+  deleteTodosByCategory
+}
 
 export default todoActions
