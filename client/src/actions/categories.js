facd8948995c074e11ad6f5e0ce50ad6ef64ff52
ghemsley <ghemsley@protonmail.ch>
2021-08-01 11:46:08 -0500

    Add category fetch actions

--- client/src/actions/categories.js
+++ client/src/actions/categories.js
@@ -1,4 +1,17 @@
 import actions from './index'
+import constants from '../constants'
+
+const fetchCategory = payload => () => {
+  return fetch(`${constants.urls.BASE_URL}/categories/${payload.id}`, {
+    headers: { Accept: 'application/json' }
+  }).then(response => response.json())
+}
+
+const fetchCategories = () => () => {
+  return fetch(`${constants.urls.BASE_URL}/categories`, {
+    headers: { Accept: 'application/json' }
+  }).then(response => response.json())
+}
 
 const createCategory = payload => ({
   type: 'CREATE_CATEGORY',
@@ -32,6 +45,8 @@ const deleteCategoriesByProject = payload => (dispatch, getState) => {
 }
 
 const categoryActions = {
+  fetchCategory,
+  fetchCategories,
   createCategory,
   deleteCategory,
   deeplyDeleteCategory,
