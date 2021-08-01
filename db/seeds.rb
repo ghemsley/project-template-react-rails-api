7e6899bd996214755c83eaf24ae62884d904b88e
ghemsley <ghemsley@protonmail.ch>
2021-07-31 22:06:46 -0500

    Update Project model, add seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+projects = Project.create([{ name: 'Final project', description: 'Phase 5 project built with Rails, React and Redux' },
+                           { name: 'Job search', description: 'Things I need to do to find a job as a developer' }])
+
+projects.each do |project|
+  categories = Category.create([{ name: 'Needs doing', description: 'Work that still needs to be done', project: project },
+                                { name: 'In progress', description: 'Work that has already been started',
+                                  project: project },
+                                { name: 'Done', description: 'Work that has been finished', project: project }])
+  categories.each do |category|
+    Todo.create([{ name: 'Test todo', description: 'Todo or not todo, that is the question?', category: category },
+                 { name: 'Test todo 2',
+                   description: 'Test Todo: The Motion Picture: The Sequel: The Game: The Broadway Musical: The JSON Response',
+                   category: category }])
+  end
+end
