21ac841bf15cfd8fc9cd277427281941e4464b86
ghemsley <ghemsley@protonmail.ch>
2021-07-31 19:36:21 -0500

    Add modal link to todos page

--- client/src/pages/Todos.js
+++ client/src/pages/Todos.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import TodoForm from '../components/TodoForm'
 import Todo from '../components/Todo'
+import { Link, useLocation } from 'react-router-dom'
 
 const Todos = () => {
   const todos = useSelector(state => state.todos)
+  const location = useLocation()
 
   return (
     <div className='center center-text todo-page'>
       <div>
-        <p>Create Todo</p>
-        <TodoForm />
-        <p>Todos</p>
+        <Link
+          className='pure-button pure-button-primary'
+          to={{ pathname: 'todos/new', state: { background: location } }}
+        >
+          Create Todo
+        </Link>
+        {todos.length > 0 && <h1>Todos</h1>}
       </div>
       <div className='flex todo-container'>
         {todos.map(todo => (
