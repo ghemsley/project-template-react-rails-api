8d6b23b4fb554ac7a909c7aec5804ff69f5dbfde
ghemsley <ghemsley@protonmail.ch>
2021-07-31 19:35:17 -0500

    Update todo form to use modal

--- client/src/components/TodoForm.js
+++ client/src/components/TodoForm.js
@@ -3,12 +3,15 @@ import { useDispatch, useSelector } from 'react-redux'
 import actions from '../actions/index'
 import Hover from './Hover'
 import Category from './Category'
+import { Modal } from './index'
 
 const TodoForm = () => {
   const categories = useSelector(state => state.categories)
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
-  const [categoryID, setCategoryID] = useState(categories[0] ? categories[0].id : '')
+  const [categoryID, setCategoryID] = useState(
+    categories[0] ? categories[0].id : ''
+  )
   const dispatch = useDispatch()
 
   const handleSubmit = event => {
@@ -43,55 +46,65 @@ const TodoForm = () => {
   }
 
   return (
-    <>
+    <Modal>
       {categories.length < 1 ? (
-        <p>Please create a category first before trying to create a todo!</p>
+        <p className='fit margin-auto'>
+          Please create a category first before trying to create a todo!
+        </p>
       ) : (
-        <form onSubmit={handleSubmit} className='pure-form pure-form-stacked center fit'>
-          <fieldset>
-            <label htmlFor='name'>Name</label>
-            <input
-              type='text'
-              name='name'
-              value={name}
-              onChange={handleChange}
-            />
-            <label htmlFor='description'>Description</label>
-            <input
-              type='text'
-              name='description'
-              value={description}
-              onChange={handleChange}
-            />
-            <label htmlFor='categoryID' className='hoverable'>
-              Category
-              <select
-                name='categoryID'
-                value={categoryID}
+        <>
+          <h1 className='fit margin-auto'>New Todo</h1>
+          <form
+            onSubmit={handleSubmit}
+            className='pure-form pure-form-stacked fit margin-auto'
+          >
+            <fieldset>
+              <label htmlFor='name'>Name</label>
+              <input
+                type='text'
+                name='name'
+                value={name}
                 onChange={handleChange}
-              >
-                {categories.map((category, i) => (
-                  <option value={category.id} key={i}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-              {categories.length > 0 && (
-                <Hover>
-                  <Category
-                    category={categories.find(category => category.id === categoryID)}
-                    showTodos={true}
-                  />
-                </Hover>
-              )}
-            </label>
-            <button className='pure-button pure-button-primary' type='submit'>
-              Submit
-            </button>
-          </fieldset>
-        </form>
+              />
+              <label htmlFor='description'>Description</label>
+              <input
+                type='text'
+                name='description'
+                value={description}
+                onChange={handleChange}
+              />
+              <label htmlFor='categoryID' className='hoverable'>
+                Category
+                <select
+                  name='categoryID'
+                  value={categoryID}
+                  onChange={handleChange}
+                >
+                  {categories.map((category, i) => (
+                    <option value={category.id} key={i}>
+                      {category.name}
+                    </option>
+                  ))}
+                </select>
+                {categories.length > 0 && (
+                  <Hover>
+                    <Category
+                      category={categories.find(
+                        category => category.id === categoryID
+                      )}
+                      showTodos={true}
+                    />
+                  </Hover>
+                )}
+              </label>
+              <button className='pure-button pure-button-primary' type='submit'>
+                Submit
+              </button>
+            </fieldset>
+          </form>
+        </>
       )}
-    </>
+    </Modal>
   )
 }
 export default TodoForm
