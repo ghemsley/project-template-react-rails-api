a6981db6885ab47be948a53b0371cb2c1ca406ba
ghemsley <ghemsley@protonmail.ch>
2021-07-31 19:34:16 -0500

    Add modal routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
-import './App.css'
-import { Navbar } from './components/index'
+import { Route, Switch, useLocation } from 'react-router-dom'
+import { Navbar, ProjectForm, CategoryForm, TodoForm } from './components/index'
 import Pages from './pages/index'
+import './App.css'
 
 function App() {
+  const location = useLocation()
+  const background = location.state && location.state.background
+
   return (
-    <Router>
-      <DndProvider backend={HTML5Backend}>
-        <div className='pure-u-1'>
-          <Navbar />
-          <Switch>
-            {Pages.map((page, i) => (
-              <Route exact path={page.path} key={i}>
-                {page.component()}
-              </Route>
-            ))}
-          </Switch>
-        </div>
-      </DndProvider>
-    </Router>
+    <div className='pure-u-1'>
+      <Navbar />
+      <Switch location={background ? background : location}>
+        {Pages.map((page, i) => (
+          <Route exact path={page.path} key={i}>
+            {page.component()}
+          </Route>
+        ))}
+      </Switch>
+      {background && (
+        <>
+          <Route exact path={`/projects/new`} children={<ProjectForm />} />
+          <Route exact path={`/categories/new`} children={<CategoryForm />} />
+          <Route exact path={`/todos/new`} children={<TodoForm />} />
+        </>
+      )}
+    </div>
   )
 }
 
