01e278b40cd16338c937f6ab6d6d8b652999be26
ghemsley <ghemsley@protonmail.ch>
2021-08-02 18:19:30 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,29 @@ Rails.application.routes.draw do
 
   get '/categories/:id/todos', to: 'category_todos#index'
   get '/categories/:category_id/todos/:todo_id', to: 'category_todos#show'
+
   get '/categories/:id/project', to: 'category_project#show'
 
   get '/projects/:id/categories', to: 'project_categories#index'
   get '/projects/:project_id/categories/:category_id', to: 'project_categories#show'
+
   get '/projects/:id/todos', to: 'project_todos#index'
   get '/projects/:project_id/todos/:todo_id', to: 'project_todos#show'
+
+  get '/projects/:id/users', to: 'project_users#index'
+  get '/projects/:project_id/users/:user_id', to: 'project_users#show'
+
+  get '/users/:id/projects', to: 'user_projects#index_user_projects'
+  get '/users/:user_id/projects/:project_id', to: 'user_projects#show_user_project'
+
+  get '/users/:id/categories', to: 'user_categories#index'
+  get '/users/:user_id/categories/:category_id', to: 'user_categories#show'
+
+  get '/users/:id/todos', to: 'user_todos#index'
+  get '/users/:user_id/categories/:todo_id', to: 'user_todos#show'
+
+  get '/user_projects/:id/user', to: 'user_projects#show_user'
+  get '/user_projects/:id/project', to: 'user_projects#show_project'
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
