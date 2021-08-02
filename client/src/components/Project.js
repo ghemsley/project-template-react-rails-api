172c747debb7f0cf4736af7af7be6afc6c309580
ghemsley <ghemsley@protonmail.ch>
2021-08-01 22:01:49 -0500

    Integrate confirm screen with project component

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,17 +1,16 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import actions from '../actions/index'
-import { Category, Dropzone } from './index'
+import { Category, Dropzone, ConfirmScreen } from './index'
 
 const Project = props => {
   const categories = useSelector(state =>
     state.categories.filter(category => category.projectID === props.project.id)
   )
   const dispatch = useDispatch()
+  const [showConfirmScreen, setShowConfirmScreen] = useState(false)
+
   const style = useMemo(() => ({}), [])
-  const handleClick = () => {
-    dispatch(actions.deeplyDeleteProject(props.project))
-  }
   const handleDrop = (item, cursor, coordinates) => {
     if (item.projectID !== props.project.id) {
       dispatch(
@@ -21,6 +20,15 @@ const Project = props => {
         })
       )
     }
+  }
+  const handleClick = () => {
+    setShowConfirmScreen(true)
+  }
+  const closeAction = () => {
+    setShowConfirmScreen(false)
+  }
+  const confirmRemove = () => {
+    dispatch(actions.removeProject(props.project))
   }
 
   return (
@@ -37,12 +45,25 @@ const Project = props => {
         <h2>{props.project.name}</h2>
         <p>{props.project.description}</p>
         {props.showDelete && (
-          <button
-            className='pure-button pure-button-primary animate-height'
-            onClick={handleClick}
-          >
-            Delete
-          </button>
+          <>
+            <button
+              className='pure-button pure-button-primary animate-height'
+              onClick={handleClick}
+            >
+              Delete
+            </button>
+            {showConfirmScreen && (
+              <ConfirmScreen closeAction={closeAction}>
+                <h1>Confirm delete?</h1>
+                <button
+                  className='pure-button pure-button-primary'
+                  onClick={confirmRemove}
+                >
+                  Delete
+                </button>
+              </ConfirmScreen>
+            )}
+          </>
         )}
         {props.showCategories && (
           <>
