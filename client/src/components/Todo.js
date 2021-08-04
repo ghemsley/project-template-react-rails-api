237d0a2ee5330b7a5eb8c857477d55a4f06d8654
ghemsley <ghemsley@protonmail.ch>
2021-08-04 10:20:15 -0500

    Update todo component with coordinates functions

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,27 +1,96 @@
-import React, { useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useDrag } from 'react-dnd'
 import actions from '../actions/index'
 import { ConfirmScreen } from './index'
 import { Link, useLocation } from 'react-router-dom'
+import mergeRefs from 'react-merge-refs'
+import useResizeObserver from 'use-resize-observer'
+import { debounce } from 'lodash'
 
 const Todo = props => {
-  const dispatch = useDispatch()
   const category = useSelector(state =>
     state.categories.find(category => category.id === props.todo.categoryID)
   )
+  const todoCoordinates = useSelector(state =>
+    state.coordinates.find(
+      coords => coords.type === 'todo' && coords.item.id === props.todo.id
+    )
+  )
+  // const [lastDragPos, setLastDragPos] = useState(null)
+  // const [lastDragCursorPos, setLastDragCursorPos] = useState(null)
   const [showConfirmScreen, setShowConfirmScreen] = useState(false)
+  const [order, setOrder] = useState(0)
+  const dispatch = useDispatch()
   const location = useLocation()
+  const ref = useRef(null)
 
-  const [{ dragging, coordinates }, drag, dragPreview] = useDrag(() => ({
-    type: 'TODO',
-    item: props.todo,
-    previewOptions: { captureDraggingState: true },
-    collect: monitor => ({
-      dragging: monitor.isDragging(),
-      coordinates: monitor.getSourceClientOffset()
-    })
-  }))
+  const getCoordinates = useCallback(
+    size => {
+      if (ref.current) {
+        console.log('resize todo')
+        const rect = ref.current.getBoundingClientRect()
+        const coordinates = {
+          type: 'todo',
+          item: props.todo,
+          position: {
+            left: rect.left,
+            right: rect.right,
+            top: rect.top,
+            bottom: rect.bottom,
+            width: size.width,
+            height: size.height,
+            rectWidth: rect.width,
+            rectHeight: rect.height
+          }
+        }
+        dispatch(actions.refreshCoordinates(coordinates))
+      }
+    },
+    [props.todo, dispatch]
+  )
+
+  const onResize = debounce(size => getCoordinates(size), 100, {
+    maxWait: 250,
+    trailing: true
+  })
+  const onScroll = debounce(size => getCoordinates(size), 250, {
+    maxWait: 500,
+    trailing: true
+  })
+
+  const observer = useResizeObserver({ ref, onResize })
+
+  useEffect(() => {
+    document.addEventListener('scroll', () =>
+      onScroll({ width: observer.width, height: observer.height })
+    )
+    return () => {
+      document.removeEventListener('scroll', () =>
+        onScroll({ width: observer.width, height: observer.height })
+      )
+    }
+  }, [])
+
+  const [{ dragging, cursor, element }, drag, dragPreview] = useDrag(
+    () => ({
+      type: 'todo',
+      item: monitor => props.todo,
+      previewOptions: { captureDraggingState: true },
+      options: { dropEffect: 'copy' },
+      collect: monitor => ({
+        dragging: monitor.isDragging(),
+        cursor: monitor.getClientOffset(),
+        element: monitor.getSourceClientOffset()
+      }),
+      end: (item, monitor) => {
+        if (monitor.didDrop()) {
+          console.log('result', monitor.getDropResult())
+        }
+      }
+    }),
+    []
+  )
 
   const style = useMemo(
     () => ({ background: dragging ? '#10a0f0' : '#1090d0' }),
@@ -40,14 +109,14 @@ const Todo = props => {
 
   return (
     <div
-      id={props.todo.id}
+      id={`todo-${props.todo.id}`}
       className='hoverable flex-child rounded todo'
       style={{
         color: 'whitesmoke',
         transition: 'background-color 150ms ease',
         ...style
       }}
-      ref={drag}
+      ref={dragging ? mergeRefs([ref, dragPreview]) : mergeRefs([ref, drag])}
     >
       <h2>{props.todo.name}</h2>
       <p>{props.todo.description}</p>
