cfe9b6eaef481d7e89ceda26a59b2bebda41b1bb
ghemsley <ghemsley@protonmail.ch>
2021-08-04 10:19:53 -0500

    Update dropzone with coordinates functionality

--- client/src/components/Dropzone.js
+++ client/src/components/Dropzone.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDrop } from 'react-dnd'
+import { useSelector } from 'react-redux'
 
 const Dropzone = props => {
-  const [{ isOver, canDrop, cursor, type, coordinates }, drop] = useDrop({
-    accept: ['TODO', 'CATEGORY', 'PROJECT'],
+  const coordinates = useSelector(state => state.coordinates)
+  const [
+    { isOver, canDrop, cursor, type, dragElementOffset, item, delta },
+    drop
+  ] = useDrop({
+    accept: ['todo', 'category', 'project'],
     drop: (item, monitor) => {
-      props.handleDrop(item, cursor, coordinates)
+      const childCoordinates = coordinates.find(coords => {
+        switch (props.parentCoordinates.type) {
+          case 'project':
+            return coords.type === 'category' && coords.item.id === item.id
+
+          case 'category':
+            return coords.type === 'todo' && coords.item.id === item.id
+
+          default:
+            return null
+        }
+      })
+      const result = {
+        child: childCoordinates,
+        parent: props.parentCoordinates,
+        cursor: monitor.getClientOffset(),
+        element: monitor.getSourceClientOffset(),
+        delta: monitor.getDifferenceFromInitialOffset()
+      }
+      props.handleDrop(item, result)
+      return result
     },
     canDrop: (item, monitor) => {
-      return type === props.acceptType ? true : false
+      return type === props.acceptType
     },
     collect: monitor => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop(),
       type: monitor.getItemType(),
       cursor: monitor.getClientOffset(),
-      coordinates: monitor.getSourceClientOffset()
+      dragElementOffset: monitor.getSourceClientOffset(),
+      delta: monitor.getDifferenceFromInitialOffset(),
+      item: monitor.getItem()
     })
   })
   return (
-    <div ref={drop} className='height-100 width-100'>
+    <div ref={drop} className='dropzone'>
       {props.children}
     </div>
   )
