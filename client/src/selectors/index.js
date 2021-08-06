d5fb1005c708b1b70b4a010058e6d71152a87a71
ghemsley <ghemsley@protonmail.ch>
2021-08-06 11:20:40 -0500

    Add redux-views, memoized selectors

---
+++ client/src/selectors/index.js
@@ -0,0 +1,79 @@
+import { createSelector, createIdSelector } from 'redux-views'
+
+const selectCoordinates = state => state.coordinates
+
+const selectAcceptType = createIdSelector(props => props.acceptType)
+
+const selectParentType = createIdSelector(props => props.parentType)
+
+const selectParentID = createIdSelector(props => props.parentID)
+
+const filterCoordinates = (coordinates, acceptType, parentType, parentID) =>
+  coordinates
+    .filter(
+      coords =>
+        coords.type === acceptType &&
+        coords.item[`${parentType}ID`] === parentID
+    )
+    .sort((coords1, coords2) => coords1.item.order - coords2.item.order)
+
+export const makeSelectChildCoordinates = () =>
+  createSelector(
+    [selectCoordinates, selectAcceptType, selectParentType, selectParentID],
+    filterCoordinates
+  )
+
+const selectProjects = state => state.projects
+
+const selectCategoryProjectID = createIdSelector(
+  props => props.category.projectID
+)
+
+const findProjectByCategoryProjectID = (projects, projectID) =>
+  projects.find(project => project.id === projectID)
+
+export const makeSelectProjectByCategoryProjectID = createSelector(
+  [selectProjects, selectCategoryProjectID],
+  findProjectByCategoryProjectID
+)
+
+const selectTodos = state => state.todos
+
+const selectCategoryID = createIdSelector(props => props.category.id)
+
+const filterTodosByCategoryID = (todos, categoryID) => {
+  const compareOrder = (todo1, todo2) => todo1.order - todo2.order
+  return todos.filter(todo => todo.categoryID === categoryID).sort(compareOrder)
+}
+
+export const makeSelectTodosByCategoryID = createSelector(
+  [selectTodos, selectCategoryID],
+  filterTodosByCategoryID
+)
+
+const selectCategories = state => state.categories
+
+const selectTodoCategoryID = createIdSelector(props => props.todo.categoryID)
+
+const findCategoryByTodoCategoryID = (categories, todoCategoryID) =>
+  categories.filter(category => category.id === todoCategoryID)
+
+export const makeSelectCategoryByTodoCategoryID = createSelector(
+  [selectCategories, selectTodoCategoryID],
+  findCategoryByTodoCategoryID
+)
+
+const selectProjectID = createIdSelector(props => props.project.id)
+
+const filterCategoriesByProjectId = (categories, projectID) => {
+  const compareOrder = (category1, category2) =>
+    category1.order - category2.order
+  return categories
+    .filter(category => category.projectID === projectID)
+    .sort(compareOrder)
+}
+
+export const makeSelectCategoriesByProjectID = createSelector(
+  [selectCategories, selectProjectID],
+  filterCategoriesByProjectId
+)
