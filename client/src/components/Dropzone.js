6e7a411229312bfd287e2bd50268b463e805bb1e
ghemsley <ghemsley@protonmail.ch>
2021-08-06 11:21:47 -0500

    Convert handleDrop to generic form in Dropzone

--- client/src/components/Dropzone.js
+++ client/src/components/Dropzone.js
@@ -1,48 +1,92 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useDrop } from 'react-dnd'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { makeSelectChildCoordinates } from '../selectors'
+import actions from '../actions/'
 
-const Dropzone = props => {
-  const coordinates = useSelector(state => state.coordinates)
-  const [
-    { isOver, canDrop, cursor, type, dragElementOffset, item, delta },
-    drop
-  ] = useDrop({
-    accept: ['todo', 'category', 'project'],
-    drop: (item, monitor) => {
-      const childCoordinates = coordinates.find(coords => {
-        switch (props.parentCoordinates.type) {
-          case 'project':
-            return coords.type === 'category' && coords.item.id === item.id
-
-          case 'category':
-            return coords.type === 'todo' && coords.item.id === item.id
-
-          default:
-            return null
+const Dropzone = React.memo(props => {
+  const selectChildCoordinates = useMemo(makeSelectChildCoordinates, [props])
+  const childCoordinates = useSelector(state =>
+    selectChildCoordinates(state, props)
+  )
+  const dispatch = useDispatch()
+  const handleDrop = useCallback(
+    (item, result) => {
+      let order = 0
+      let i = 0
+      while (i < childCoordinates.length) {
+        if (childCoordinates[i].item.id !== item.id) {
+          if (result.element.y < childCoordinates[i].position.top) {
+            order = i
+            for (let j = i; j < childCoordinates.length; j++) {
+              if (childCoordinates[j].item.id !== item.id) {
+                childCoordinates[j].item.order += 1
+              }
+            }
+            if (i === 0) {
+              break
+            }
+            for (let j = i - 1; j >= 0; j--) {
+              if (childCoordinates[j].item.id !== item.id) {
+                childCoordinates[j].item.order -= 1
+              }
+            }
+            break
+          } else {
+            order = i + 1
+          }
         }
-      })
-      const result = {
-        child: childCoordinates,
-        parent: props.parentCoordinates,
-        cursor: monitor.getClientOffset(),
-        element: monitor.getSourceClientOffset(),
-        delta: monitor.getDifferenceFromInitialOffset()
+        i++
       }
-      props.handleDrop(item, result)
+      const childrenToUpdate = childCoordinates.map(coords => coords.item)
+      const existingChild = childrenToUpdate.find(
+        existing => existing.id === item.id
+      )
+      if (!existingChild) {
+        const child = { ...item, order: order }
+        child[`${props.parentType}ID`] = props.parentID
+        childrenToUpdate.push(child)
+      } else {
+        childrenToUpdate[childrenToUpdate.indexOf(existingChild)].order = order
+      }
+      childrenToUpdate.sort((child1, child2) => child1.order - child2.order)
+      for (let i = 0; i < childrenToUpdate.length; i++) {
+        childrenToUpdate[i].order = i
+      }
+      switch (props.acceptType) {
+        case 'todo':
+          dispatch(actions.batchAmendTodos(childrenToUpdate))
+          break
+
+        case 'category':
+          dispatch(actions.batchAmendCategories(childrenToUpdate))
+          break
+
+        default:
+          break
+      }
+    },
+    [
+      props.parentID,
+      props.parentType,
+      props.acceptType,
+      childCoordinates,
+      dispatch
+    ]
+  )
+
+  const [{ type }, drop] = useDrop({
+    accept: [props.acceptType],
+    drop: (item, monitor) => {
+      const result = { element: monitor.getSourceClientOffset() }
+      handleDrop(item, result)
       return result
     },
-    canDrop: (item, monitor) => {
+    canDrop: () => {
       return type === props.acceptType
     },
     collect: monitor => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-      type: monitor.getItemType(),
-      cursor: monitor.getClientOffset(),
-      dragElementOffset: monitor.getSourceClientOffset(),
-      delta: monitor.getDifferenceFromInitialOffset(),
-      item: monitor.getItem()
+      type: monitor.getItemType()
     })
   })
   return (
@@ -50,6 +94,6 @@ const Dropzone = props => {
       {props.children}
     </div>
   )
-}
+})
 
 export default Dropzone
