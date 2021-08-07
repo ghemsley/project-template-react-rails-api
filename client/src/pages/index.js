4198b8a433d86f12009808f018e708ab91aa16f7
ghemsley <ghemsley@protonmail.ch>
2021-08-06 23:33:39 -0500

    Update pages index to use protected property

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,10 +4,15 @@ import Todos from './Todos'
 import Projects from './Projects'
 
 const Pages = [
-  { name: 'Home', component: Home, path: '/' },
-  { name: 'Projects', component: Projects, path: '/projects' },
-  { name: 'Categories', component: Categories, path: '/categories' },
-  { name: 'Todos', component: Todos, path: '/todos' }
+  { name: 'Home', component: Home, path: '/', protected: false },
+  { name: 'Projects', component: Projects, path: '/projects', protected: true },
+  {
+    name: 'Categories',
+    component: Categories,
+    path: '/categories',
+    protected: true
+  },
+  { name: 'Todos', component: Todos, path: '/todos', protected: true }
 ]
 
 export { Home, Projects, Categories, Todos }
