4da947bbaa8d2df2fa473d5f8f13588ed2d89f48
ghemsley <ghemsley@protonmail.ch>
2021-08-06 23:30:06 -0500

    Add loading component

--- client/src/components/Dropzone.js
+++ client/src/components/Dropzone.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { useDrop } from 'react-dnd'
 import { useDispatch, useSelector } from 'react-redux'
 import { makeSelectChildCoordinates } from '../selectors'
---
+++ client/src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ConfirmScreen } from '.'
+
+const Loading = () => {
+  return (
+    <ConfirmScreen>
+      <p className='spin'>Loading...</p>
+    </ConfirmScreen>
+  )
+}
+
+export default Loading
