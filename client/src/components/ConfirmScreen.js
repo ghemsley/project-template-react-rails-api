c0c2c05e1fa586fc336ef12ccdc4139c100eefaf
ghemsley <ghemsley@protonmail.ch>
2021-08-06 23:29:47 -0500

    Update confirm screen component

--- client/src/components/ConfirmScreen.js
+++ client/src/components/ConfirmScreen.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const ConfirmScreen = props => {
+  const history = useHistory()
   const preventClose = event => {
     event.stopPropagation()
   }
   return (
-    <div className='pure-g modal' onClick={props.closeAction}>
+    <div
+      className='pure-g modal'
+      onClick={props.closeAction ? props.closeAction : history.goBack}>
       <div className='pure-u-1 modal-child rounded' onClick={preventClose}>
-        <span className='modal-close-button' onClick={props.closeAction}>
+        <span
+          className='modal-close-button'
+          onClick={props.closeAction ? props.closeAction : history.goBack}>
           ×
         </span>
         {props.children}
