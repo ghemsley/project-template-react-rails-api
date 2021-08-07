337896430c6e0239195b3846ac45f8d2c1891d24
ghemsley <ghemsley@protonmail.ch>
2021-08-06 23:30:54 -0500

    Update navbar with user management links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,53 @@
 import { Link } from 'react-router-dom'
 import Pages from '../pages/index'
-
-const Navbar = () => (
-  <div className='center fit pure-menu pure-menu-horizontal pure-menu-scrollable'>
-    <ul className='pure-menu-list'>
-      {Pages.map((page, i) => (
-        <li className='pure-menu-item' key={i}>
-          <Link to={page.path} className='pure-menu-link'>
-            {page.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-)
+import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router'
+const Navbar = () => {
+  const { authChecked, loggedIn, currentUser } = useSelector(
+    state => state.authentication
+  )
+  const location = useLocation()
+  return (
+    <div className='center fit pure-menu pure-menu-horizontal pure-menu-scrollable'>
+      <ul className='pure-menu-list'>
+        {Pages.map((page, i) => (
+          <li className='pure-menu-item' key={i}>
+            <Link to={page.path} className='pure-menu-link'>
+              {page.name}
+            </Link>
+          </li>
+        ))}
+        {!loggedIn && (
+          <>
+            <li className='pure-menu-item' key={'signup'}>
+              <Link
+                className='pure-menu-link'
+                to={{ pathname: 'signup', state: { background: location } }}>
+                Sign up
+              </Link>
+            </li>
+            <li className='pure-menu-item' key={'login'}>
+              <Link
+                className='pure-menu-link'
+                to={{ pathname: 'login', state: { background: location } }}>
+                Login
+              </Link>
+            </li>
+          </>
+        )}
+        {authChecked && loggedIn && currentUser && (
+          <li className='pure-menu-item' key={'logout'}>
+            <Link
+              className='pure-menu-link'
+              to={{ pathname: 'logout', state: { background: location } }}>
+              Logout
+            </Link>
+          </li>
+        )}
+      </ul>
+      <div className='button-container'></div>
+    </div>
+  )
+}
 
 export default Navbar
