500f7e3b8bfc24541ab3b0365e774c68b5b0810a
ghemsley <ghemsley@protonmail.ch>
2021-08-10 18:12:18 -0500

    Fix batch update route, move fallback controller

--- app/controllers/api/fallback_controller.rb
+++ app/controllers/api/fallback_controller.rb
@@ -4,6 +4,6 @@ class FallbackController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     # React app index page
-    render file: 'public/index.html'
+    render file: 'client/public/index.html'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,30 @@ Rails.application.routes.draw do
   namespace :api do
     resources :user_projects, only: %i[index show create destroy]
     resources :users, only: %i[index show create update destroy]
-    resources :todos, only: %i[index show create update destroy]
-    resources :categories, only: %i[index show create update destroy]
-    resources :projects, only: %i[index show create update destroy]
+    resources :todos, only: %i[index show create update destroy] do
+      collection do
+        patch 'batch_update'
+      end
+    end
+    resources :categories, only: %i[index show create update destroy] do
+      collection do
+        patch 'batch_update'
+      end
+    end
+    resources :projects, only: %i[index show create update destroy] do
+      collection do
+        patch 'batch_update'
+      end
+    end
   end
 
   get '/api/current_user', to: 'api/current_user#index'
 
-  patch '/api/todos/batch_update', to: 'api/todos#batch_update'
-  patch '/api/categories/batch_update', to: 'api/categories#batch_update'
-  patch '/api/projects/batch_update', to: 'api/projects#batch_update'
+  # patch '/api/todos/batch_update', to: 'api/todos#batch_update'
+  # patch '/api/categories/batch_update', to: 'api/categories#batch_update'
+  # patch '/api/projects/batch_update', to: 'api/projects#batch_update'
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '/api/*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
