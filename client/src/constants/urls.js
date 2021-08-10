bbd8de08bb5181c381dffb5233a7159ed916b51b
ghemsley <ghemsley@protonmail.ch>
2021-08-10 12:43:27 -0500

    Set base url based on node environment

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,5 +1,8 @@
 const URLS = {
-  BASE_URL: 'http://localhost:3000'
+  BASE_URL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://tada-todo-list.herokuapp.com'
+      : 'http://localhost:3000'
 }
 
 export default URLS
