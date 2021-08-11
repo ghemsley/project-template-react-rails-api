065bcc713dfbd401af5a5c8016738b8427c457f1
ghemsley <ghemsley@protonmail.ch>
2021-08-11 14:03:04 -0500

    Fix fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,26 @@ Rails.application.routes.draw do
     sessions: 'api/users/sessions',
     registrations: 'api/users/registrations'
   }
+  get '/api/current_user', to: 'api/current_user#index'
   namespace :api do
-    resources :user_projects, only: %i[index show create destroy]
-    resources :users, only: %i[index show create update destroy]
-    resources :todos, only: %i[index show create update destroy] do
+    resources :user_projects, only: %i[show create destroy]
+    resources :users, only: %i[show create update destroy]
+    resources :todos, only: %i[create update destroy] do
       collection do
         patch 'batch_update'
       end
     end
-    resources :categories, only: %i[index show create update destroy] do
+    resources :categories, only: %i[create update destroy] do
       collection do
         patch 'batch_update'
       end
     end
-    resources :projects, only: %i[index show create update destroy] do
+    resources :projects, only: %i[index create update destroy] do
       collection do
         patch 'batch_update'
       end
     end
   end
-
-  get '/api/current_user', to: 'api/current_user#index'
 
   # patch '/api/todos/batch_update', to: 'api/todos#batch_update'
   # patch '/api/categories/batch_update', to: 'api/categories#batch_update'
@@ -35,5 +34,5 @@ Rails.application.routes.draw do
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get '/api/*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
