046d93e7fb343b5f7fd64370da68f0f0705a0ef6
ghemsley <ghemsley@protonmail.ch>
2021-08-19 15:00:14 -0500

    Refactor authentication actions

--- client/src/actions/authentication.js
+++ client/src/actions/authentication.js
@@ -2,13 +2,86 @@ import CONSTANTS from '../constants'
 import helpers from '../helpers'
 
 const setToken = token => {
-  const currentTime = new Date(Date.now()).getTime()
   localStorage.setItem('token', token)
-  localStorage.setItem('lastLogin', currentTime.toString())
 }
 
 const getToken = () => {
   return localStorage.getItem('token')
+}
+
+const signup = credentials => {
+  return fetch(`${CONSTANTS.URLS.BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user: credentials })
+  }).then(res => {
+    if (res.ok) {
+      setToken(res.headers.get('Authorization'))
+      return helpers.convertIdToInt(res.json())
+    } else {
+      return helpers
+        .convertIdToInt(res.json())
+        .then(error => Promise.reject(error))
+    }
+  })
+}
+
+const login = credentials =>
+  fetch(`${CONSTANTS.URLS.BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user: credentials })
+  }).then(res => {
+    if (res.ok) {
+      setToken(res.headers.get('Authorization'))
+      return helpers.convertIdToInt(res.json())
+    } else {
+      return helpers
+        .convertIdToInt(res.json())
+        .then(error => Promise.reject(error))
+    }
+  })
+
+const logout = () =>
+  fetch(`${CONSTANTS.URLS.BASE_URL}/logout`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: getToken()
+    }
+  }).then(res => {
+    if (res.ok) {
+      return helpers.convertIdToInt(res.json())
+    } else {
+      return helpers
+        .convertIdToInt(res.json())
+        .then(error => Promise.reject(error))
+    }
+  })
+
+const authorize = () => {
+  return fetch(`${CONSTANTS.URLS.BASE_URL}/current_user`, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: getToken()
+    }
+  }).then(res => {
+    if (res.ok) {
+      return helpers.convertIdToInt(res.json())
+    } else {
+      return helpers
+        .convertIdToInt(res.json())
+        .then(error => Promise.reject(error))
+    }
+  })
 }
 
 const authenticateUser = payload => {
@@ -33,93 +106,48 @@ const unauthenticateUser = () => ({
 })
 
 const signupUser = credentials => dispatch =>
-  fetch(`${CONSTANTS.URLS.BASE_URL}/signup`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ user: credentials })
-  }).then(res => {
-    if (res.ok) {
-      setToken(res.headers.get('Authorization'))
-      return helpers.convertIdToInt(res.json()).then(json => {
-        dispatch(authenticateUser(json))
-        return json
-      })
-    } else {
-      return helpers.convertIdToInt(res.json()).then(error => {
-        dispatch(unauthenticateUser())
-        return Promise.reject(error)
-      })
-    }
-  })
+  signup(credentials)
+    .then(json => {
+      dispatch(authenticateUser(json))
+      return json
+    })
+    .catch(error => {
+      dispatch(unauthenticateUser())
+      return Promise.reject(error)
+    })
 
 const loginUser = credentials => dispatch =>
-  fetch(`${CONSTANTS.URLS.BASE_URL}/login`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ user: credentials })
-  }).then(res => {
-    if (res.ok) {
-      setToken(res.headers.get('Authorization'))
-      return helpers.convertIdToInt(res.json()).then(json => {
-        dispatch(authenticateUser(json))
-        return json
-      })
-    } else {
-      return helpers.convertIdToInt(res.json()).then(error => {
-        dispatch(unauthenticateUser())
-        return Promise.reject(error)
-      })
-    }
-  })
+  login(credentials)
+    .then(json => {
+      dispatch(authenticateUser(json))
+      return json
+    })
+    .catch(error => {
+      dispatch(unauthenticateUser())
+      return Promise.reject(error)
+    })
 
 const logoutUser = () => dispatch =>
-  fetch(`${CONSTANTS.URLS.BASE_URL}/logout`, {
-    method: 'DELETE',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: getToken()
-    }
-  }).then(res => {
-    if (res.ok) {
-      return helpers.convertIdToInt(res.json()).then(json => {
-        dispatch(unauthenticateUser())
-        return json
-      })
-    } else {
-      return helpers.convertIdToInt(res.json()).then(error => {
-        dispatch(unauthenticateUser())
-        return Promise.reject(error)
-      })
-    }
-  })
+  logout()
+    .then(json => {
+      dispatch(unauthenticateUser())
+      return json
+    })
+    .catch(error => {
+      dispatch(unauthenticateUser())
+      return Promise.reject(error)
+    })
 
 const checkAuth = () => dispatch =>
-  fetch(`${CONSTANTS.URLS.BASE_URL}/current_user`, {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: getToken()
-    }
-  }).then(res => {
-    if (res.ok) {
-      return helpers.convertIdToInt(res.json()).then(json => {
-        dispatch(authenticateUser(json))
-        return json
-      })
-    } else {
-      return helpers.convertIdToInt(res.json()).then(error => {
-        dispatch(unauthenticateUser())
-        return Promise.reject(error)
-      })
-    }
-  })
+  authorize()
+    .then(json => {
+      dispatch(authenticateUser(json))
+      return json
+    })
+    .catch(error => {
+      dispatch(unauthenticateUser())
+      return Promise.reject(error)
+    })
 
 const authenticationActions = {
   setToken,
