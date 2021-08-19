bf50bf47bd42cbf5a92773f34e958b67e7c9b30a
ghemsley <ghemsley@protonmail.ch>
2021-08-19 15:01:36 -0500

    Add delete coordinates action

--- client/src/reducers/coordinates.js
+++ client/src/reducers/coordinates.js
@@ -5,19 +5,30 @@ const coordinates = (state = [], action) => {
   if (action.payload) {
     payload = helpers.convertIdToInt(action.payload)
   }
+  const newState = [...state]
   switch (action.type) {
     case 'REFRESH_COORDINATES':
       const coords = state.find(
         coords =>
-          coords.type === payload.type &&
-          coords.item.id === payload.item.id
+          coords.type === payload.type && coords.item.id === payload.item.id
       )
       if (coords && coords.position !== payload.position) {
-        const newState = [...state]
         newState[newState.indexOf(coords)] = payload
         return newState
+      } else if (!coords) {
+        return [...newState, payload]
+      } else {
+        return state
       }
-      return [...state, payload]
+
+    case 'DELETE_COORDINATES':
+      const coordinate = newState.find(
+        coord =>
+          coord.item.id === payload.item.id && coord.type === payload.type
+      )
+      if (coordinate) {
+        return newState.splice(newState.indexOf(coordinate), 1)
+      } else return state
 
     default:
       return state
